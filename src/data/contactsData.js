10dccda4bf2f1c192c72837c4493756f1d843e86
galang721 <ajaweeg@gmail.com>
2025-06-24 22:44:34 +0700

    Initial commit

---
+++ src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'galanglk72@gmail.com',
+    phone: '+6285136110830',
+    address: 'WP69+HH8 Tanjungwangi, Kabupaten Bandung, Jawa Barat ',
+
+    sheetAPI: ''
+}
---
+++ src/data/educationData.js
@@ -0,0 +1,23 @@
+export const educationData = [
+    {
+        id: 1,
+        institution: ' Al Muawanah Islamic Boarding School',
+        course: 'Junior High School',
+        startYear: '2019',
+        endYear: '2020'
+    },
+    {
+        id: 2,
+        institution: 'Al Azhar',
+        course: ' Senior High School',
+        startYear: '2022',
+        endYear: '2023'
+    },
+    {
+        id: 3,
+        institution: 'Bale Bandung University',
+        course: 'Students Of Informatics engineering',
+        startYear: '2024',
+        endYear: 'Present'
+    },
+]
